c8cda2540d215ab39e9182acc435724dee121299
rosgaard-zz <thomas@rosgaard.net>
2019-02-22 12:10:32 +0100

    Update httpTest.ts

--- httpTest.ts
+++ httpTest.ts
@@ -2,30 +2,10 @@
 Obloq.Obloq_http_setup(
     SerialPin.P1,
     SerialPin.P2,
-    "dfrobotYanfa",
-    "hidfrobot",
-    "192.168.0.106",
-    8080
+    "yourSSID",
+    "yourPASSWORD"
     )
     basic.forever(() => {
-        basic.showString(Obloq.Obloq_http_get("input?id=1&val=" + input.lightLevel(), 10000))
-        basic.pause(50)
-        basic.showString(Obloq.Obloq_http_post("input?name=admin", "{\"id\":\"1\",\"val\":\"" + input.temperature() + "\"}", 10000))
-        basic.pause(50)
+        basic.showString(Obloq.Obloq_http_post("4b7ff593-bf90-4109-acba-f6a331c36bfa", "100", 10000))
+        basic.pause(60000)
     })
-    
-//IFTTT
-Obloq.Obloq_http_setup(
-    SerialPin.P1,
-    SerialPin.P2,
-    "dfrobotYanfa",
-    "hidfrobot",
-    "maker.ifttt.com",
-    80
-    )
-    basic.showString(Obloq.Obloq_wifi_ipconfig())
-    basic.forever(() => {
-        basic.showString(Obloq.Obloq_http_get("trigger/testobloq/with/key/h_srC8EuWGX5izhfi7pi5", 10000))
-        basic.pause(100)
-    })
-
