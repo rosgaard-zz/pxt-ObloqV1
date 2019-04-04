2975be21b68d1a650a0aa400293ac2ed28cc60b1
rosgaard-zz <thomas@rosgaard.net>
2019-04-04 22:46:37 +0200

    Add files via upload

--- httpTest.ts
+++ httpTest.ts
@@ -2,10 +2,30 @@
 Obloq.Obloq_http_setup(
     SerialPin.P1,
     SerialPin.P2,
-    "yourSSID",
-    "yourPASSWORD"
+    "dfrobotYanfa",
+    "hidfrobot",
+    "192.168.0.106",
+    8080
     )
     basic.forever(() => {
-        basic.showString(Obloq.Obloq_http_post("4b7ff593-bf90-4109-acba-f6a331c36bfa", "100", 10000))
-        basic.pause(60000)
+        basic.showString(Obloq.Obloq_http_get("input?id=1&val=" + input.lightLevel(), 10000))
+        basic.pause(50)
+        basic.showString(Obloq.Obloq_http_post("input?name=admin", "{\"id\":\"1\",\"val\":\"" + input.temperature() + "\"}", 10000))
+        basic.pause(50)
     })
+    
+//IFTTT
+Obloq.Obloq_http_setup(
+    SerialPin.P1,
+    SerialPin.P2,
+    "dfrobotYanfa",
+    "hidfrobot",
+    "maker.ifttt.com",
+    80
+    )
+    basic.showString(Obloq.Obloq_wifi_ipconfig())
+    basic.forever(() => {
+        basic.showString(Obloq.Obloq_http_get("trigger/testobloq/with/key/h_srC8EuWGX5izhfi7pi5", 10000))
+        basic.pause(100)
+    })
+
