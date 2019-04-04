5a7619bc98384e5cb99981e5610799c431045e64
rosgaard-zz <thomas@rosgaard.net>
2019-04-05 01:02:36 +0200

    Update httpTest.ts

--- httpTest.ts
+++ httpTest.ts
@@ -3,9 +3,7 @@ Obloq.Obloq_http_setup(
     SerialPin.P1,
     SerialPin.P2,
     "dfrobotYanfa",
-    "hidfrobot",
-    "192.168.0.106",
-    8080
+    "hidfrobot"
     )
     basic.forever(() => {
         basic.showString(Obloq.Obloq_http_get("4b7ff593-bf90-4109-acba-f6a331c36bfa", 10000))
